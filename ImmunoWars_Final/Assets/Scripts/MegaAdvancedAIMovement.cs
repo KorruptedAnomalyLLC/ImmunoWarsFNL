50b1ea861c8918045eb8e73066ce434dcc58518d
Joshua Watts <joshuawatts3@gmail.com>
2020-07-13 18:54:10 -0600

    -updates on AI movement

---
+++ ImmunoWars_Final/Assets/Scripts/MegaAdvancedAIMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaAdvancedAIMovement : MonoBehaviour
+{
+    [SerializeField]
+    private bool useRandomMovement = false;
+    [SerializeField]
+    private bool chasingTarget = false;
+
+    [SerializeField]
+    private RandomPointGenerator randMoveScript;
+
+
+
+    private void MoveYoAss()
+    {
+        if (useRandomMovement)
+        {
+           
+        }
+    }
+}
