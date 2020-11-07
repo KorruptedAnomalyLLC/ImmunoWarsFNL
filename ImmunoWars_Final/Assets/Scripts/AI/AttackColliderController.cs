0b02d27ade782b57708e3be768b6cda76c032a59
JoshuaWatts <joshuawatts3@gmail.com>
2020-11-06 20:20:30 -0700

    -Added simple combat for the player

---
+++ ImmunoWars_Final/Assets/Scripts/AI/AttackColliderController.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class AttackColliderController : MonoBehaviour
+{
+    private int damageAmount = 2;
+    private HealthManager healthScript;
+    private Collider myCollider;
+
+    private void Start()
+    {
+        myCollider = GetComponentInParent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider == myCollider)
+            return;
+
+
+
+        healthScript = collider.gameObject.GetComponentInParent<HealthManager>();
+
+        Debug.Log("Made it into Trigger " + healthScript);
+
+        if (healthScript != null)
+        {
+            healthScript.TakeDamage(damageAmount);
+        }
+    }
+}
--- ImmunoWars_Final/Assets/Scripts/AI/CombatRoot.cs
+++ ImmunoWars_Final/Assets/Scripts/AI/CombatRoot.cs
@@ -4,5 +4,96 @@ using UnityEngine;
 
 public class CombatRoot : MonoBehaviour
 {
+
+    private LocalBlackboard _localBlackboard;
+    public float attackRechargeTime = 10f;
+    private float counter = 0f;
+
+    public bool attackReady = true;
+
+    public void Setup()
+    {
+        _localBlackboard = GetComponent<LocalBlackboard>();
+    }
+
+
+    private void Update()
+    {
+        Recharge();
+    }
+
+    public void _update()
+    {
+        CheckRange();
+
+        switch (_localBlackboard._attack)
+        {
+            case Attack.Slot1:
+                BasicTestAttack();
+                break;
+            case Attack.Slot2:
+                break;
+            case Attack.Slot3:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public GameObject basicHitbox;
+    private Coroutine tempCoroutine;
+    private void BasicTestAttack()
+    {
+        if(_localBlackboard.inRange && attackReady)
+        {
+            attackReady = false;
+            basicHitbox.SetActive(true);
+
+            if(tempCoroutine == null)
+                tempCoroutine = StartCoroutine(ResetAttack());
+        }
+    }
+
+    public float hitTime = 0.6f;
+    private IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(hitTime);
+        basicHitbox.SetActive(false);
+
+    }
+
     
+
+
+    private void Recharge()
+    {
+        if (attackReady)
+            return;
+
+        counter += Time.deltaTime;
+        if (counter >= attackRechargeTime)
+        {
+            attackReady = true;
+            tempCoroutine = null;
+            counter = 0;
+        }
+
+    }
+
+
+    public float attackSlopDistance = 2f;
+    private void CheckRange()
+    {
+        Vector3 TempPos = _localBlackboard.currentTarget.position;
+
+        if((TempPos - transform.position).magnitude < attackSlopDistance + _localBlackboard.targetMovementOffset)
+        {
+            _localBlackboard.inRange = true;
+        }
+        else
+        {
+            _localBlackboard.inRange = false;
+        }
+    }
 }
